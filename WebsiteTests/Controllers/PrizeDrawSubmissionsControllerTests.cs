6049f01e746e066084500baee29e0c8ee1a742bd
Dinh Tran <mrtrandinhvn@gmail.com>
2025-12-11 22:30:45 +0700

    Update controller unit tests

--- WebsiteTests/Controllers/PrizeDrawSubmissionsControllerTests.cs
+++ WebsiteTests/Controllers/PrizeDrawSubmissionsControllerTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Application.Common;
+using Application.Services.PrizeDrawSubmissions.Read;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Website.Controllers;
 
@@ -8,18 +9,45 @@ namespace WebsiteTests.Controllers
     public class PrizeDrawSubmissionsControllerTests
     {
         [Fact]
-        public void Index_Should_Returns_AViewResult()
+        public async Task Index_Should_Returns_AViewResult()
         {
-            // Arrange
-            var mockRepo = new Mock<ILogger<HomeController>>();
-            var controller = new HomeController(mockRepo.Object);
+            // setup
+            var input = new GetPaginatedSubmissionsInput
+            {
+                Page = 1,
+                PageSize = 10,
+            };
+            var mockRepo = new Mock<IPrizeDrawSubmissionReadRepository>();
+            mockRepo.Setup(x => x.GetPaginatedResultAsync(input))
+            .ReturnsAsync(new PaginationOutput<ProductPrizeDrawSubmissionReadModel>
+            {
+                CurrentPage = 1,
+                Data = new List<ProductPrizeDrawSubmissionReadModel>
+                {
+                        new ProductPrizeDrawSubmissionReadModel
+                        {
+                            Id = 1,
+                            OwnerEmail = string.Empty,
+                            OwnerFirstName = string.Empty,
+                            OwnerLastName = string.Empty,
+                            ProductSerialNumber = string.Empty,
+                        }
+                },
+                TotalRecords = 10,
+            });
+            var controller = new PrizeDrawSubmissionsController(mockRepo.Object);
 
-            // Act
-            var result = controller.Index();
+            // execution
+            var result = await controller.Index(input);
 
-            // Assert
+            // asserts
             var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<PaginationOutput<ProductPrizeDrawSubmissionReadModel>>(viewResult.ViewData.Model);
             Assert.NotNull(viewResult);
+            Assert.NotNull(model);
+            Assert.Equal(1, model.CurrentPage);
+            Assert.Equal(10, model.TotalRecords);
+            Assert.Single(model.Data);
         }
     }
 }
